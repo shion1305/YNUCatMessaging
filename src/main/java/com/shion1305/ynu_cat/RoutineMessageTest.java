b9f5e9c43f2fe1c9cea303625a89eddefcd2964c
Shion Ichikawa <shion1305@gmail.com>
2022-01-02 23:06:33 +0900

    First Phase Development

--- src/main/java/com/shion1305/ynu_cat/RoutineMessageTest.java
+++ src/main/java/com/shion1305/ynu_cat/RoutineMessageTest.java
@@ -4,18 +4,15 @@ import com.shion1305.ynu_cat.message.MessageSchedule;
 import com.shion1305.ynu_cat.message.MessageType;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class RoutineMessageTest {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         System.out.println("STARTED");
         calendar.add(Calendar.SECOND, 5);
-        for (int i=0; i<10; i++) {
-            calendar.add(Calendar.SECOND, 1);
-            Member member = new Member("Ua54a5505fcbf303b59a856c6bbec5518");
-            MessageSchedule mes = new MessageSchedule(MessageType.ROUTINE_NOTIFICATION, calendar.getTime(), member, null);
-            MessageScheduleManager.schedule(mes);
-        }
+        calendar.add(Calendar.SECOND, 1);
+        Member member = new Member("Ua54a5505fcbf303b59a856c6bbec5518");
+        MessageSchedule mes = new MessageSchedule(MessageType.ROUTINE_NOTIFICATION, calendar.getTime(), member, null);
+        MessageScheduleManager.schedule(mes);
     }
 }
